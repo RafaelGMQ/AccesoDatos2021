dd2ac8d86537e7f9ae7e6af2238797c3906c3b84
Rafael <rquintana.profesor@gmail.com>
2020-11-26 17:12:25 +0100

    Lectura de ficheros de caracteres byte a byte

---
+++ src/main/java/com/rjqdf/teoria/ficheros/A_accesosecuencial/B_ficheros_de_caracteres/A_byte_a_byte/A_basico/Lectura.java
@@ -0,0 +1,35 @@
+package com.rjqdf.teoria.ficheros.A_accesosecuencial.B_ficheros_de_caracteres.A_byte_a_byte.A_basico;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Lectura {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File(".\\input\\input.bin");
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+
+            int i;
+            StringBuilder readMessage = new StringBuilder();
+            while ((i = inputStreamReader.read()) != -1) {
+
+                readMessage.append((char) i);
+            }
+
+            inputStreamReader.close();
+
+            System.out.println(readMessage);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
