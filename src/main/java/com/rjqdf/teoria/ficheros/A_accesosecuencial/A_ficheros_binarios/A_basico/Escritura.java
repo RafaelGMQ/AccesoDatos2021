4512e24645da513263b0cb8361f5344d915198cb
Rafael <rquintana.profesor@gmail.com>
2020-11-26 16:27:35 +0100

    Lectura y escritura en ficheros binarios

---
+++ src/main/java/com/rjqdf/teoria/ficheros/A_accesosecuencial/A_ficheros_binarios/A_basico/Escritura.java
@@ -0,0 +1,32 @@
+package com.rjqdf.teoria.ficheros.A_accesosecuencial.A_ficheros_binarios.A_basico;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Escritura {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File(".\\output\\output-FileOutputStream.bin");
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            byte[] array = new byte[256];
+
+            for (int i = 0; i < array.length; i++) {
+
+                array[i] = (byte) i;
+            }
+
+            fileOutputStream.write(array);
+
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
