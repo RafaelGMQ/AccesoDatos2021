7bc667adfd75259f302de008a586ad7f7e17fb37
Rafael <rquintana.profesor@gmail.com>
2020-11-26 08:57:48 +0100

    Solución ejercicio 3

---
+++ src/main/java/com/rjqdf/ejercicios/ejercicio3/BufferedReaderTest.java
@@ -0,0 +1,29 @@
+package com.rjqdf.ejercicios.ejercicio3;
+
+import java.io.IOException;
+
+public class BufferedReaderTest {
+
+    public static void main(String[] args) {
+
+        try {
+
+            BufferedReader bufferedReader = new BufferedReader(".\\input\\quijote.txt");
+
+            String readMessage;
+            int i = 0;
+            while ((readMessage = bufferedReader.readLine()) != null){
+
+                System.out.println(++i + ". " + readMessage);
+            }
+
+            System.out.println(bufferedReader.readLine());
+
+            bufferedReader.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/rjqdf/ejercicios/ejercicio3/PropertiesTest.java
@@ -0,0 +1,27 @@
+package com.rjqdf.ejercicios.ejercicio3;
+
+import java.io.IOException;
+
+public class PropertiesTest {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Properties configurationFile = new Properties(".\\output\\configuration.props");
+
+            configurationFile.setConfiguration("user", "Rafael");
+            configurationFile.setConfiguration("age", "20");
+            configurationFile.setConfiguration("home", "C:\\users\\Rafael\\");
+
+            System.out.println(configurationFile.getConfiguration("user"));
+            configurationFile.deleteConfiguration("user");
+            System.out.println(configurationFile.getConfiguration("user"));
+//            configurationFile.deleteFile();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
